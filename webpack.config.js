2b75e34aa3499de4f79d0d20d8ff62d62f9a4ec6
Dylan Sather <dylan.sather@gmail.com>
2017-12-25 16:08:13 -0700

    More boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,24 @@ module.exports = {
   context: path.join(__dirname, '/src'),
   entry: './app',
   output: {
-    path: path.join(__dirname, '/build'),
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loaders: ['eslint-loader', 'babel-loader'],
+        loaders: 'babel-loader',
       },
     ],
   },
   resolve: {
-    modules: [path.resolve('./src'), path.resolve('./node_modules')],
+    modules: [
+      path.join(__dirname, '/config'),
+      path.join(__dirname, '/src'),
+      path.join(__dirname, '/node_modules'),
+    ],
+    extensions: ['.js', '.jsx'],
   },
 };
